b1d950a081d0780207bcc4fb1cba2ebe6a023b27
Wang1758 <Wang1758@email.com>
2024-11-29 22:35:30 +0800

    更改了路径

--- src/TestOnline.cc
+++ src/TestOnline.cc
@@ -6,7 +6,7 @@ using namespace wd;
 
 int main() {
     Configuration::getInstance(
-        "/home/wjj/桌面/search/Forum-SearchEnigine/conf/online.conf");
+        "/home/wjj/桌面/search/SearchEnigine/conf/online.conf");
 
     WordQueryServer server(stoi(CONFIG["threadNum"]), stoi(CONFIG["queSize"]),
                            CONFIG["ip"], stoi(CONFIG["port"]));
